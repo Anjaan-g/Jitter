0c115a9fd0b1caec88a2fb50b853ae17036c4bd3
Anjaan <anjaangaire@gmail.com>
2019-05-01 12:44:49 +0545

    Templates and Static files added.

--- Jitter/posts/views.py
+++ Jitter/posts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
 
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name= 'posts/index.html'
+    def get_queryset(self):
+        return True
