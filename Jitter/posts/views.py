ccd3a19544771a697cb760958187c25d5a779a30
Anjaan <anjaangaire@gmail.com>
2019-05-02 19:15:02 +0545

    Login page created separately.

--- Jitter/posts/views.py
+++ Jitter/posts/views.py
@@ -7,3 +7,7 @@ class IndexView(generic.ListView):
     template_name= 'posts/index.html'
     def get_queryset(self):
         return True
+class LoginView(generic.ListView):
+    template_name = 'posts/login.html'
+    def get_queryset(self):
+        return True
