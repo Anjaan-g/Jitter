6600c6461f76d1d9e2606d44a4e1e88ecb6bbc3a
Anjaan <anjaangaire@gmail.com>
2019-05-05 08:34:14 +0545

    User authentication created.

--- Jitter/posts/models.py
+++ Jitter/posts/models.py
@@ -4,12 +4,12 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Posts(models.Model):
-    post_text = models.CharField(max_length = 500)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.post_text
-    def was_published_recently(self):
-        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now    
+# class Posts(models.Model):
+#     post_text = models.CharField(max_length = 500)
+#     pub_date = models.DateTimeField('date published')
+#     def __str__(self):
+#         return self.post_text
+#     def was_published_recently(self):
+#         # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+#         now = timezone.now()
+#         return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- Jitter/posts/views.py
+++ Jitter/posts/views.py
@@ -1,13 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
-
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout
+)
+from .forms import UserLoginForm, UserRegisterForm
+#
+# def login_view(request):
+#     next = request.GET.get('next')
+#     form = UserLoginForm(request.POST or None)
+#     if form.is_valid():
+#         username = form.cleaned_data.get('username')
+#         password = form.cleaned_data.get('password')
+#         user = authenticate(username=username, password=password)
+#         login(request, user)
+#         if next:
+#             return redirect(next)
+#         return redirect('/')
+#
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, "login.html",context)
 
 # Create your views here.
+
 class IndexView(generic.ListView):
-    template_name= 'posts/index.html'
+    template_name = 'posts/index.html'
     def get_queryset(self):
         return True
+
+
 class LoginView(generic.ListView):
     template_name = 'posts/login.html'
+
+    def login_view(request):
+        next = request.GET.get('next')
+        form = UserLoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            if next:
+                return redirect(next)
+            return redirect('/')
+
+        context = {
+            'form': form,
+        }
+        return render(request, 'posts/login.html',context)
+
+
     def get_queryset(self):
         return True
