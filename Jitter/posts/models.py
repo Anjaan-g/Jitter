6c341e6c570b496609e11fa33566d90e8c10dd06
Anjaan <anjaangaire@gmail.com>
2019-05-16 15:56:51 +0545

    created model and installed PIL.

--- Jitter/posts/models.py
+++ Jitter/posts/models.py
@@ -1,15 +1,17 @@
+from django.conf import settings
+
 import datetime
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
 
-# class Posts(models.Model):
-#     post_text = models.CharField(max_length = 500)
-#     pub_date = models.DateTimeField('date published')
-#     def __str__(self):
-#         return self.post_text
-#     def was_published_recently(self):
-#         # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-#         now = timezone.now()
-#         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+class Post(models.Model):
+    user = models.ForeignKey(User, default =1, null=True, on_delete=models.SET_NULL )
+    image = models.ImageField(upload_to='image/',null=True, blank = True)
+    post_text = models.CharField(max_length = 500)
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-pub_date']
