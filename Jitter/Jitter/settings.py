26560779346f30b6f37d3223540fab679a52116c
Anjaan <anjaangaire@gmail.com>
2019-05-17 12:49:21 +0545

    user login signup successfully created.

--- Jitter/Jitter/settings.py
+++ Jitter/Jitter/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Jitter.urls'
+LOGIN_REDIRECT_URL = '/posts/'
+LOGOUT_REDIRECT_URL = '/posts/'
 
 TEMPLATES = [
     {
--- Jitter/posts/urls.py
+++ Jitter/posts/urls.py
@@ -20,7 +20,7 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.IndexView.as_view(), name = 'index'),
-    path('login/',views.LoginView.as_view(),name = 'login'),
-    path('signup/',views.RegisterView.as_view(),name = 'signup'),
-    path('logout/',views.LogoutView.as_view(),name = 'logout')
+    # path('login/',views.LoginView.as_view(),name = 'login'),
+    path('signup/',views.SignUp.as_view(),name = 'signup'),
+    # path('logout/',views.LogoutView.as_view(),name = 'logout')
 ]
